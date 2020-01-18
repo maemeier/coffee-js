0acdabe4dbd10c71dc884d35e3522448d9f135bc
Mae Kangsadan <maemeier@live.com>
2020-01-18 18:23:16 +0100

    video controller

--- js/app.js
+++ js/app.js
@@ -2,11 +2,41 @@
 
 // window even list
 
-window.addEventListener("load", function() {
-  document.querySelector(".preloader").style.display = "none";
-});
+addEventListeners();
+function addEventListeners() {
+  const ui = new UI();
+  window.addEventListener("load", function() {
+    ui.hidePreloader();
+  });
 
-// nav btn
-document.querySelector(".navBtn").addEventListener("click", function() {
+  // nav btn
+  document.querySelector(".navBtn").addEventListener("click", function() {
+    ui.showNav();
+  });
+
+  // turn video
+  document
+    .querySelector(".video__switch")
+    .addEventListener("click", function() {
+      ui.videoControls();
+    });
+}
+
+function UI() {}
+
+UI.prototype.hidePreloader = function() {
+  document.querySelector(".preloader").style.display = "none";
+};
+UI.prototype.showNav = function() {
   document.querySelector(".nav").classList.toggle("nav--show");
-});
+};
+UI.prototype.videoControls = function() {
+  let btn = document.querySelector(".video__switch-btn");
+  if (!btn.classList.contains("btnSlide")) {
+    btn.classList.add("btnSlide");
+    document.querySelector(".video__item").pause();
+  } else {
+    btn.classList.remove("btnSlide");
+    document.querySelector(".video__item").play();
+  }
+};
